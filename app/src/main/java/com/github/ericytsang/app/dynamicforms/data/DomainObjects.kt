a16b14be2aadfe59df9f7385c1a397d8a7b1a3bf
Eric Tsang <surplus.et@gmail.com>
2019-08-31 15:32:23 -0700

    fixed room DB

--- app/src/main/java/com/github/ericytsang/app/dynamicforms/data/DomainObjects.kt
+++ app/src/main/java/com/github/ericytsang/app/dynamicforms/data/DomainObjects.kt
@@ -1,8 +1,20 @@
 package com.github.ericytsang.app.dynamicforms.data
 
+import android.util.Patterns
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import java.io.File
+
+// domain objects
+data class LocalFile(val filePath: File)
+{
+    init
+    {
+        require(filePath.exists()) {"file does not exist: $filePath"}
+    }
+}
 
 @Entity
 data class Form(
@@ -17,21 +29,31 @@ sealed class FormField
     abstract val formId:Long
     abstract val isRequired:Boolean
 
-    @Entity
+    @Entity(foreignKeys = [
+        ForeignKey(
+            entity = Form::class,
+            parentColumns = ["id"],
+            childColumns = ["formId"],
+            onDelete = ForeignKey.CASCADE)])
     data class TextFormField(
             @PrimaryKey(autoGenerate = true)
             override val id:Long,
-            @ForeignKey(entity = Form::class)
+            @ColumnInfo(index = true)
             override val formId:Long,
             override val isRequired:Boolean,
             val value:String)
         :FormField()
 
-    @Entity
+    @Entity(foreignKeys = [
+        ForeignKey(
+            entity = Form::class,
+            parentColumns = ["id"],
+            childColumns = ["formId"],
+            onDelete = ForeignKey.CASCADE)])
     data class DateFormField(
             @PrimaryKey(autoGenerate = true)
             override val id:Long,
-            @ForeignKey(entity = Form::class)
+            @ColumnInfo(index = true)
             override val formId:Long,
             override val isRequired:Boolean,
             val value:Long)
