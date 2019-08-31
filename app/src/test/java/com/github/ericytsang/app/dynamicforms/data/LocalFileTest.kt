df071c42ebedab6e06e53286c769ddeb0edf951b
Eric Tsang <surplus.et@gmail.com>
2019-08-31 16:23:06 -0700

    added placeholder test for LocalFile

---
+++ app/src/test/java/com/github/ericytsang/app/dynamicforms/data/LocalFileTest.kt
@@ -0,0 +1,16 @@
+package com.github.ericytsang.app.dynamicforms.data
+
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+// todo: mock the filesystem!?
+@RunWith(RobolectricTestRunner::class)
+class LocalFileTest
+{
+    @Test
+    @Ignore("todo: mock the filesystem!?")
+    fun mock_the_filesystem()
+    {}
+}
