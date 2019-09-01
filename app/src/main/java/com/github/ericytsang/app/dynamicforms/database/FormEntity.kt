5e93206ad683a03ccf29b21c681ac3174280ce11
Eric Tsang <surplus.et@gmail.com>
2019-09-01 00:34:10 -0700

    separate DAO and Entity classes into different files

--- app/src/main/java/com/github/ericytsang/app/dynamicforms/database/FormEntity.kt
+++ app/src/main/java/com/github/ericytsang/app/dynamicforms/database/FormEntity.kt
@@ -1,38 +1,8 @@
 package com.github.ericytsang.app.dynamicforms.database
 
-import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Embedded
 import androidx.room.Entity
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.PrimaryKey
-import androidx.room.Query
-import androidx.room.Update
-
-
-@Dao
-abstract class FormDao
-{
-    @Insert(entity = FormEntity::class)
-    protected abstract fun _insert(values:FormEntity.Values):Long
-
-    fun insert(values:FormEntity.Values) = FormEntity.Pk(_insert(values))
-
-    @Query("SELECT * FROM FormEntity")
-    abstract fun selectAll():List<FormEntity>
-
-    @Query("SELECT * FROM FormEntity WHERE id = :id")
-    protected abstract fun _selectOne(id:Long):FormEntity?
-
-    fun selectOne(pk:FormEntity.Pk) = _selectOne(pk.id)
-
-    @Update(entity = FormEntity::class,onConflict = OnConflictStrategy.ABORT)
-    abstract fun update(form:FormEntity)
-
-    @Delete(entity = FormEntity::class)
-    abstract fun delete(pk:FormEntity.Pk)
-}
 
 
 @Entity
--- app/src/main/java/com/github/ericytsang/app/dynamicforms/database/ImageEntity.kt
+++ app/src/main/java/com/github/ericytsang/app/dynamicforms/database/ImageEntity.kt
@@ -1,31 +1,8 @@
 package com.github.ericytsang.app.dynamicforms.database
 
-import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Embedded
 import androidx.room.Entity
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.PrimaryKey
-import androidx.room.Query
-
-
-@Dao
-abstract class ImageDao
-{
-    @Insert(entity = ImageEntity::class,onConflict = OnConflictStrategy.REPLACE)
-    abstract fun insert(values:ImageEntity)
-
-    @Query("SELECT * FROM ImageEntity")
-    abstract fun selectAll():List<ImageEntity>
-
-    @Query("SELECT * FROM ImageEntity WHERE url = :url")
-    protected abstract fun _selectOne(url:String):ImageEntity?
-    fun selectOne(pk:ImageEntity.Pk) = _selectOne(pk.url)
-
-    @Delete(entity = ImageEntity::class)
-    abstract fun delete(pk:ImageEntity.Pk)
-}
 
 
 @Entity
