ec0d21821c20a9214b876623d1a33ffe389bf14f
Eric Tsang <surplus.et@gmail.com>
2019-09-02 12:52:28 -0700

    update extensions to do type checkig, an d delegate to contextcopat

--- app/src/main/java/com/github/ericytsang/app/dynamicforms/Extensions.kt
+++ app/src/main/java/com/github/ericytsang/app/dynamicforms/Extensions.kt
@@ -3,27 +3,26 @@ package com.github.ericytsang.app.dynamicforms
 import android.app.Activity
 import android.content.Context
 import android.graphics.drawable.Drawable
-import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import kotlin.reflect.KClass
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 
 
 // Context
 
 val Context.layoutInflater get() = LayoutInflater.from(this)
 
-fun Context.getDrawableCompat(drawableResId:Int):Drawable
+fun Context.getDrawableCompat(@DrawableRes drawableResId:Int):Drawable
 {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-    {
-        resources.getDrawable(drawableResId,theme)
-    } else
-    {
-        @Suppress("DEPRECATION")
-        resources.getDrawable(drawableResId)
-    }
+    return ContextCompat.getDrawable(this,drawableResId)!!
+}
+
+fun Context.getColorCompat(@ColorRes colorResId:Int):Int
+{
+    return ContextCompat.getColor(this,colorResId)
 }
 
 // Activity
